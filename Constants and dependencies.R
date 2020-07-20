3b32dbee8d60ab348aafcf4d8ff048f8908cea0b
Ronald Sokoloff <rsokoloff@gmail.com>
2020-07-20 02:32:54 +0000

    Import FIPS & JHU data, process, and join.

--- Constants and dependencies.R
+++ Constants and dependencies.R
@@ -21,4 +21,13 @@ US_cases_deceased_URL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID
 rolling_mean_time <- 5L # days
 rolling_mean_time_new_dead <- 5L # days
 recovery_time <- 14L # Days
+VISN <- 9
 #########
+
+non_state <- c("Guam", "Samoa", "Micronesia", "Islands", "Philippines",
+               "Palau", "Overseas", "Puerto"
+               )
+
+
+
+
