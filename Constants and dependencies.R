2353f0ef6d7022b4b3b3fbfe9fed4121275aaad1
Ronald Sokoloff <rsokoloff@gmail.com>
2020-07-21 01:45:37 +0000

    Import, clean, and mutate COVID Project data.

--- Constants and dependencies.R
+++ Constants and dependencies.R
@@ -16,7 +16,7 @@ sapply(packages, library, character.only = TRUE)
 
 US_cases_confirmed_URL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 US_cases_deceased_URL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
-
+COVID_project_URL <- "https://covidtracking.com/api/v1/states/daily.csv"
 #########
 rolling_mean_time <- 5L # days
 rolling_mean_time_new_dead <- 5L # days
